9d080acef55ccdeae76daefec93fb4fb3d0137ee
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-18 23:38:19 +0530

    doc: Add onPressed event on flatButton widget

--- Exercise-IV/lib/main.dart
+++ Exercise-IV/lib/main.dart
@@ -22,13 +22,14 @@ class DicePage extends StatelessWidget {
       child:Row(
         children: <Widget>[
           Expanded(
-            child: FlatButton(child: Image.asset('images/dice1.png')),
+            child: FlatButton(
+                onPressed: () {
+                  print("Left Button got pressed");
+                },
+                child: Image.asset('images/dice1.png')),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice2.png'),
-            ),
+            child: FlatButton(child: Image.asset('images/dice2.png')),
           ),
         ],
       ),
