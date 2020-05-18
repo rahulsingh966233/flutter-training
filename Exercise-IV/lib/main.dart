0c285309cdd531e8db6e6391a5747022100c97c0
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-18 23:14:22 +0530

    doc: Wrap Expanded widget with padding

--- Exercise-IV/lib/main.dart
+++ Exercise-IV/lib/main.dart
@@ -19,13 +19,19 @@ class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child:Row(
         children: <Widget>[
-          Expanded(
-            child: Image.asset('images/dice1.png'),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Expanded(
+              child: Image.asset('images/dice1.png'),
+            ),
           ),
-          Expanded(
-            child: Image.asset('images/dice2.png'),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Expanded(
+              child: Image.asset('images/dice2.png'),
+            ),
           ),
         ],
       ),
