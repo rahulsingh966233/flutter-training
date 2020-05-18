16d1b19e200e9997a9da2ebcb06b2870d9e280d4
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-18 20:25:37 +0530

    doc: Add card for phone with style

--- Exercise-III/lib/main.dart
+++ Exercise-III/lib/main.dart
@@ -12,8 +12,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
+            child:Column(children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
                 backgroundImage: AssetImage('images/icon.png'),
@@ -21,14 +20,37 @@ class MyApp extends StatelessWidget {
               Text(
                 'Shubhangi Ambade',
                 style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold
               ),
-            ],
-          ),
-        ),
+              ),
+              Text("FLUTTER DEVELOPER",style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),),
+              SizedBox(height: 20.0,width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone,color: Colors.white,),
+                  title: Text('+44 123 456 789', style: TextStyle(
+                    color: Colors.teal.shade900,
+                    fontSize: 20.0,
+                    fontFamily: 'Source Sans Pro',
+                  ),),
+                )
+              ),
+            ],),
+      ),
       ),
     );
   }
