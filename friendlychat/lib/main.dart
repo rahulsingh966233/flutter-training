6aba9807963de5f5b26a0fdc3782ea09db997df8
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-19 17:35:20 +0530

    doc: Add _buildTextComposer function

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -27,6 +27,17 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
+
+  Widget _buildTextComposer(){
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
