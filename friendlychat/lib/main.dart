2bf088b9d87e95c1b2ae1033944458bd04260b01
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-19 17:30:21 +0530

    doc: Change stateless widget to stateful for chatscreen

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -20,7 +20,12 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
