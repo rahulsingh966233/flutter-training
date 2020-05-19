c7fedcb4b3b3b8ca7aad1e4ca464be51fe9f14cd
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-19 23:18:07 +0530

    doc: Add theme property into FriendlyChatApp class

--- friendlychat/lib/main.dart
+++ friendlychat/lib/main.dart
@@ -2,29 +2,31 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final ThemeData kIOSTheme = ThemeData(
-      primarySwatch: Colors.orange,
-      primaryColor: Colors.grey[100],
-      primaryColorBrightness: Brightness.light);
-  final ThemeData kDefaultTheme = ThemeData(
-    primarySwatch: Colors.purple,
-    accentColor: Colors.orangeAccent[400],
-  );
   runApp(
     FriendlyChatApp(),
   );
 }
 
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange,
+    primaryColor: Colors.grey[100],
+    primaryColorBrightness: Brightness.light);
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
 class FriendlyChatApp extends StatelessWidget {
   const FriendlyChatApp({
     Key key,
   }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FriendlyChat',
-      
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
       home: ChatScreen(),
     );
   }
