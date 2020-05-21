9b2f77b96faaf7adb80c255b9451984961e09900
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-21 19:49:38 +0530

    fix: Re-implement varibles with constructor

--- quizzler-app/lib/main.dart
+++ quizzler-app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quiz());
 
@@ -26,13 +27,12 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scorekeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
 
-  List<bool> answers = [false, true, true];
+  List<Question> questionBank = [
+  Question( q:'You can lead a cow down stairs but not up stairs,', a: false),
+  Question( q:'Approximately one quarter of human bones are in the feet.', a: true),
+  Question( q:'A slug\'s blood is green.', a: true),
+  ];
 
   int questionNumber = 0;
 
