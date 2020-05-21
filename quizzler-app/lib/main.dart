d3947d626fba27492a0b1cc256a65116c3e73de9
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-21 17:05:57 +0530

    refactor: Create list variable for Icons

--- quizzler-app/lib/main.dart
+++ quizzler-app/lib/main.dart
@@ -25,6 +25,29 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scorekeeper = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -84,18 +107,8 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-
         Row(
-          children: <Widget>[
-            Icon(
-            Icons.check,
-                color:Colors.green,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            )
-          ],
+          children: scorekeeper,
         )
       ],
     );
