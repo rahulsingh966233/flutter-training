e4dd4e0a00c9b098cc6f11cafe650b407c32f1fb
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-21 17:32:57 +0530

    feat: Add questionNumber varibale to dynamic change

--- quizzler-app/lib/main.dart
+++ quizzler-app/lib/main.dart
@@ -32,6 +32,8 @@ class _QuizPageState extends State<QuizPage> {
     'A slug\'s blood is green.'
   ];
 
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -45,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
               padding: EdgeInsets.all(10.0),
               child: Center(
                 child: Text(
-                  questions[0],
+                  questions[questionNumber],
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 25.0,
