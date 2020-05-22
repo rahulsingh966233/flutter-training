9fd6289b0dc136ddc2275b241fcc1bcdc8f759a6
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-22 20:43:09 +0530

    feat: Implement Wrap widget

--- widgets_of_the_week/lib/main.dart
+++ widgets_of_the_week/lib/main.dart
@@ -8,7 +8,21 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Wrap(
+            alignment: WrapAlignment.end,
+            spacing: 10.0,
+            runSpacing: 20.0,
+            children: [
+              Chip(
+                avatar: CircleAvatar(backgroundColor: Colors.blue.shade900, child: Text('AH')),
+                label: Text('Hamilton'),
+              ),
+              Chip(
+                avatar: CircleAvatar(backgroundColor: Colors.blue.shade900, child: Text('ML')),
+                label: Text('Lafayette'),
+              ),
+            ],
+          ),
         ),
       ),
     );
