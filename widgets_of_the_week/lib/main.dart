209f9afd233cdd9225f318e498d53bcf70b05195
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-23 20:32:32 +0530

    feat: Implement Spacer widget

--- widgets_of_the_week/lib/main.dart
+++ widgets_of_the_week/lib/main.dart
@@ -1,58 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:xylophone/sizes_helpers.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sizes... sizes',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Sizes... sizes'),
+    return new MaterialApp(
+      title: 'MediaQuery Demo',
+      home: new MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('Size = ' + MediaQuery.of(context).size.toString());
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              width: displayWidth(context) * 0.5,
-              height: displayHeight(context) -
-                  MediaQuery.of(context).padding.top -
-                  kToolbarHeight,
-              child: Text(
-                'Box width 50% of screen width and text size 6% of screen width and full s'
-                    'creen height minus status bar height and AppBar height',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: displayWidth(context) * 0.06),
-              ),
-            ),
-          ],
-        ),
+    var size = MediaQuery.of(context).size.width;
+    return new Container(
+      child: Row(
+        children: <Widget>[
+          Container(color: Colors.red, child: Text("Left")),
+          Spacer(),
+          Container(color: Colors.red, child: Text("Right")),
+        ],
       ),
     );
   }
