229cb6b5b30b808bc1b514f8d88da5404b512f35
Shubhangi Ambade <shubhamade6897@gmail.com>
2020-05-25 17:42:58 +0530

    feat: Implement Aspect ratio widget

--- widgets_of_the_week/lib/main.dart
+++ widgets_of_the_week/lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
-//https://fluttercentral.com/Articles/Post/1211/AnimatedIcon_Example_in_Flutter
-
 void main() => runApp(LogoApp());
 
 class LogoApp extends StatefulWidget {
@@ -10,48 +8,21 @@ class LogoApp extends StatefulWidget {
 }
 
 class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-  bool isPlaying = false;
-
-  Animation animation;
-
-  AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = AnimationController(
-        duration: const Duration(milliseconds: 500), vsync: this);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-            child: IconButton(
-              iconSize: 70,
-              icon: AnimatedIcon(
-                icon: AnimatedIcons.play_pause,
-                progress: controller,
-              ),
-              onPressed: () => _onpressed(),
-            )
+        body:  Align(
+          child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AspectRatio(
+                  aspectRatio: 3 / 2,
+                  child: Text('shubh'),
+                ),
+              )),
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  _onpressed() {
-    setState(() {
-      isPlaying = !isPlaying;
-      isPlaying ? controller.forward() : controller.reverse();
-    });
   }
 }
