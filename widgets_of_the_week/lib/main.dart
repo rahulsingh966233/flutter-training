4d4115c34ea5eff19fb8123d8783cf7282bb0f25
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-25 23:23:05 +0530

    feat: Implement SingleChildScrollView widget

--- widgets_of_the_week/lib/main.dart
+++ widgets_of_the_week/lib/main.dart
@@ -26,124 +26,132 @@ class _LogoAppState extends State<LogoApp> {
         appBar: AppBar(
           title: Text("Widgets Demo"),
         ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              height: 100,
-              width: 200,
-              child: AnimatedPadding(
-                child: Container(
-                  color: Colors.red,
-                ),
-                duration: const Duration(seconds: 1),
-                padding: EdgeInsets.all(padValue),
-                curve: Curves.easeInOut,
-              ),
-            ),
-            Text('Padding Value: $padValue'),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  child: Text("Add Padding"),
-                  onPressed: () {
-                    setState(() {
-                      padValue = padValue + 10;
-                    });
-                  },
-                ),
-                RaisedButton(
-                  child: Text("Remove Padding"),
-                  onPressed: () {
-                    setState(() {
-                      if (padValue != 0) padValue = padValue - 10;
-                    });
-                  },
-                )
-              ],
-            ),
-            Container(
-              child: AnimatedPadding(
-                duration: Duration(seconds: 1),
-                curve: Curves.easeInOut,
-                padding: EdgeInsets.all(0),
-                child: Container(
-                  color: Colors.red,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 100,
-              child: AnimatedSwitcher(
-                duration: const Duration(seconds: 2),
-                transitionBuilder: (Widget child, Animation<double> animation) {
-                  return ScaleTransition(child: child, scale: animation);
-                },
-                child: _myAnimatedWidget,
-              ),
-            ),
-            RaisedButton(
-              child: Text('Click here'),
-              onPressed: () {
-                setState(() {
-                  _myAnimatedWidget =
-                      (_myAnimatedWidget.toString() == "FirstWidget")
-                          ? SecondWidget()
-                          : FirstWidget();
-                });
-              },
-            ),
-            ConstrainedBox(
-              constraints: BoxConstraints(
-                  minHeight: 50, maxHeight: 100, maxWidth: 200, minWidth: 200),
-              child: RaisedButton(
-                child: Text(
-                  'Tap Me',
-                ),
-              ),
-            ),
-
-            Stack(
-              children: <Widget>[
-                Container(
-                  color: Colors.red,
-                  height: 100,
-                  width: 100,
-                ),
-                Positioned(
-                  bottom: 20,
-                  right: 20,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 100,
+                width: 200,
+                child: AnimatedPadding(
                   child: Container(
-                    color: Colors.green,
-                    width: 80,
-                    height: 80,
+                    color: Colors.red,
+                  ),
+                  duration: const Duration(seconds: 1),
+                  padding: EdgeInsets.all(padValue),
+                  curve: Curves.easeInOut,
+                ),
+              ),
+              Text('Padding Value: $padValue'),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text("Add Padding"),
+                    onPressed: () {
+                      setState(() {
+                        padValue = padValue + 10;
+                      });
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text("Remove Padding"),
+                    onPressed: () {
+                      setState(() {
+                        if (padValue != 0) padValue = padValue - 10;
+                      });
+                    },
+                  )
+                ],
+              ),
+              Container(
+                child: AnimatedPadding(
+                  duration: Duration(seconds: 1),
+                  curve: Curves.easeInOut,
+                  padding: EdgeInsets.all(0),
+                  child: Container(
+                    color: Colors.red,
                   ),
                 ),
-                Container(
-                  color: Colors.blue,
-                  height: 60,
-                  width: 60,
-                )
-              ],
-              overflow: Overflow.clip,
-            ),
-            SizedBox(height: 10,),
-            AnimatedOpacity(
-              opacity: opacityLevel,
-              duration: Duration(seconds: 3),
-              child: Container(
-                width: 100,
-                child: FlutterLogo(),
               ),
-            ),
-            RaisedButton(
-              child: Text('Fade Logo'),
-              onPressed: _changeOpacity,
-            ),
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 100,
+                child: AnimatedSwitcher(
+                  duration: const Duration(seconds: 2),
+                  transitionBuilder: (Widget child, Animation<double> animation) {
+                    return ScaleTransition(child: child, scale: animation);
+                  },
+                  child: _myAnimatedWidget,
+                ),
+              ),
+              RaisedButton(
+                child: Text('Click here'),
+                onPressed: () {
+                  setState(() {
+                    _myAnimatedWidget =
+                        (_myAnimatedWidget.toString() == "FirstWidget")
+                            ? SecondWidget()
+                            : FirstWidget();
+                  });
+                },
+              ),
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                    minHeight: 50, maxHeight: 100, maxWidth: 200, minWidth: 200),
+                child: RaisedButton(
+                  child: Text(
+                    'Tap Me',
+                  ),
+                ),
+              ),
+
+              Stack(
+                children: <Widget>[
+                  Container(
+                    color: Colors.red,
+                    height: 100,
+                    width: 100,
+                  ),
+                  Positioned(
+                    bottom: 20,
+                    right: 20,
+                    child: Container(
+                      color: Colors.green,
+                      width: 80,
+                      height: 80,
+                    ),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 60,
+                    width: 60,
+                  )
+                ],
+                overflow: Overflow.clip,
+              ),
+              SizedBox(height: 10,),
+              AnimatedOpacity(
+                opacity: opacityLevel,
+                duration: Duration(seconds: 3),
+                child: Container(
+                  width: 100,
+                  child: FlutterLogo(),
+                ),
+              ),
+              RaisedButton(
+                child: Text('Fade Logo'),
+                onPressed: _changeOpacity,
+              ),
+            Container(
+              height: 200,
+              width: 100,
+              color: Colors.green,
+
+            )
+            ],
+          ),
         ),
       ),
     );
