9316dfc0b5430f39621b9b3490b96f3253936ebc
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-25 19:33:36 +0530

    refactor: Shrink the link of code for draggable

--- widgets_of_the_week/lib/main.dart
+++ widgets_of_the_week/lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,47 +10,45 @@ class LogoApp extends StatefulWidget {
 }
 
 class _LogoAppState extends State<LogoApp> {
-  List<String> name = ['S','H','U','B','H','A','N','G','I'];
+  List<String> name = ['S', 'H', 'U', 'B', 'H', 'A', 'N', 'G', 'I'];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 100,
-          ),
-          Container(
-            height: 500,
-            child: ReorderableListView(
-              onReorder: _onReorderView,
-              children: [
-                for(var item in name)
-                  ListTile(
-                    key:ValueKey(item),
-                    title: Text('Item#$item')
-                  )
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 100,
-          ),
-        ],
-      )),
-    );
-  }
-  void _onReorderView(int oldIndex, int newIndex) {
-    setState(
-          () {
-        if (newIndex > oldIndex) {
-          newIndex -= 1;
-        }
-        final String item = name.removeAt(oldIndex);
-        name.insert(newIndex, item);
-      },
+            children: <Widget>[
+              SizedBox(
+                height: 100,
+              ),
+              Container(
+                height: 500,
+                child: ReorderableListView(
+                  onReorder: (oldIndex, newIndex) {
+                    setState(
+                          () {
+                        if (newIndex > oldIndex) {
+                          newIndex -= 1;
+                        }
+                        final String item = name.removeAt(oldIndex);
+                        name.insert(newIndex, item);
+                      },
+                    );
+                  },
+                  children: [
+                    for(var item in name)
+                      ListTile(
+                          key: ValueKey(item),
+                          title: Text('Item#$item')
+                      )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+            ],
+          )),
     );
   }
 }
-
