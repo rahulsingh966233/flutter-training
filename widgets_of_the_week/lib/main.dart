09d641557201b2ca010a2f1e50faf9c71e70664b
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-25 19:59:55 +0530

    feat: Implement AnimatedSwitcher

--- widgets_of_the_week/lib/main.dart
+++ widgets_of_the_week/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-//https://medium.com/@adp4infotech4/flutter-building-a-reorderable-listview-735013719cf3
+//https://fluttercentral.com/Articles/Post/1207/AnimatedSwitcher_Widget_Example_in_Flutter
 
 void main() => runApp(LogoApp());
 
@@ -11,6 +11,7 @@ class LogoApp extends StatefulWidget {
 
 class _LogoAppState extends State<LogoApp> {
   List<String> name = ['S', 'H', 'U', 'B', 'H', 'A', 'N', 'G', 'I'];
+  Widget _myAnimatedWidget = FirstWidget();
 
   @override
   Widget build(BuildContext context) {
@@ -19,10 +20,10 @@ class _LogoAppState extends State<LogoApp> {
           body: Column(
             children: <Widget>[
               SizedBox(
-                height: 100,
+                height: 50,
               ),
               Container(
-                height: 500,
+                height: 200,
                 child: ReorderableListView(
                   onReorder: (oldIndex, newIndex) {
                     setState(
@@ -45,10 +46,60 @@ class _LogoAppState extends State<LogoApp> {
                 ),
               ),
               SizedBox(
-                height: 100,
+                height: 10,
               ),
+              Container(
+                height: 100,
+                child: AnimatedSwitcher(
+                  duration: const Duration(seconds: 2),
+                  transitionBuilder: (Widget child, Animation<double> animation) {
+                    return ScaleTransition(child: child, scale: animation);
+                  },
+                  child: _myAnimatedWidget,
+                ),
+              ),
+              RaisedButton(
+                child: Text('Click here'),
+                onPressed: () {
+                  setState(() {
+                    _myAnimatedWidget =
+                    (_myAnimatedWidget.toString() == "FirstWidget")
+                        ? SecondWidget()
+                        : FirstWidget();
+                  });
+                },
+              )
+
             ],
           )),
+    );
+  }
+}
+
+class FirstWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200.0,
+      height: 200.0,
+      child: Center(child: Text("This is First Widget")),
+      decoration: BoxDecoration(
+        color: Colors.orange,
+      ),
+    );
+  }
+}
+
+class SecondWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200.0,
+      height: 200.0,
+      child: Center(child: Text("This is Second Widget")),
+      decoration: BoxDecoration(
+        color: Colors.green,
+      ),
     );
   }
 }
