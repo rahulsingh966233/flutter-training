f8a7ba428a5e5a2dd04d97e3f7396050ed333119
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-27 21:15:34 +0530

    feat: Add description section

--- state_management_demo/lib/main.dart
+++ state_management_demo/lib/main.dart
@@ -5,7 +5,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  @override
+  Widget textSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Text(
+      'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.',
+      softWrap: true,
+    ),
+  );
   Widget titleSection = Container(
     padding: const EdgeInsets.all(32),
     child: Row(
@@ -42,9 +53,10 @@ class MyApp extends StatelessWidget {
       ],
     ),
   );
+  @override
   Widget build(BuildContext context) {
-    Color color = Theme.of(context).primaryColor;
 
+    Color color = Theme.of(context).primaryColor;
     Widget buttonSection = Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -62,7 +74,7 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter layout demo'),
         ),
         body: Column(
-          children: [titleSection, buttonSection],
+          children: [titleSection, buttonSection, textSection],
         ),
       ),
     );
