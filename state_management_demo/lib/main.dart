bdf169b565d06be9d2dcab342817a287e4a67c2f
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-27 21:29:02 +0530

    feat: Add Image widget

--- state_management_demo/lib/main.dart
+++ state_management_demo/lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
     padding: const EdgeInsets.all(32),
     child: Text(
       'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-          'Alps. Situated 1,578 meters above sea level, it is one of the '
-          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-          'half-hour walk through pastures and pine forest, leads you to the '
-          'lake, which warms to 20 degrees Celsius in the summer. Activities '
-          'enjoyed here include rowing, and riding the summer toboggan run.',
+      'Alps. Situated 1,578 meters above sea level, it is one of the '
+      'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+      'half-hour walk through pastures and pine forest, leads you to the '
+      'lake, which warms to 20 degrees Celsius in the summer. Activities '
+      'enjoyed here include rowing, and riding the summer toboggan run.',
       softWrap: true,
     ),
   );
@@ -55,7 +55,6 @@ class MyApp extends StatelessWidget {
   );
   @override
   Widget build(BuildContext context) {
-
     Color color = Theme.of(context).primaryColor;
     Widget buttonSection = Container(
       child: Row(
@@ -74,13 +73,22 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter layout demo'),
         ),
         body: Column(
-          children: [titleSection, buttonSection, textSection],
+          children: [
+            Image.network(
+              'https://images.pexels.com/photos/1571459/pexels-photo-1571459.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection
+          ],
         ),
       ),
     );
   }
 }
-
 
 Column _buildButtonColumn(Color color, IconData icon, String label) {
   return Column(
