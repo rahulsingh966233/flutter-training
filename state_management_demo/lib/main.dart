8052deb406c81812067182cd7ccdf35a6a41137b
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-28 23:10:33 +0530

    feat: Customize the todo app

--- state_management_demo/lib/main.dart
+++ state_management_demo/lib/main.dart
@@ -1,50 +1,43 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class Todo {
+class Todo{
   final String title;
   final String description;
 
   Todo(this.title, this.description);
 }
 
-void main() {
+void main(){
   runApp(MaterialApp(
-    title: 'Passing Data',
-    home: TodosScreen(
-      todos: List.generate(
-        10,
-            (i) => Todo(
-          'Todo $i',
-          'A description of what needs to be done for Todo $i',
-        ),
-      ),
+    title: 'Passin Data',
+    home:TodoScreen(
+    todos: List.generate(10, (index) => Todo('Todo$index', 'description$index')),
     ),
   ));
 }
 
-class TodosScreen extends StatelessWidget {
+class TodoScreen extends StatelessWidget {
   final List<Todo> todos;
 
-  TodosScreen({Key key, @required this.todos}) : super(key: key);
-
+  TodoScreen({Key key, @required this.todos}):super(key: key);
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todos'),
+        title: Text('Appbar'),
       ),
       body: ListView.builder(
         itemCount: todos.length,
-        itemBuilder: (context, index) {
+        itemBuilder: (context, index){
           return ListTile(
             title: Text(todos[index].title),
-            onTap: () {
+            onTap: (){
               Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailScreen(todo: todos[index]),
-                ),
+                context, MaterialPageRoute(
+                builder: (context)=> DetailScreen(todo:todos[index])
+              )
               );
             },
           );
@@ -56,19 +49,17 @@ class TodosScreen extends StatelessWidget {
 
 class DetailScreen extends StatelessWidget {
   final Todo todo;
-
-  DetailScreen({Key key, @required this.todo}) : super(key: key);
+  DetailScreen({Key key, @required this.todo}):super(key:key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(todo.title),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text(todo.description),
-      ),
+      ),body: Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Text(todo.description),
+    ),
     );
   }
 }
