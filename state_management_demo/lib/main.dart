d1c9550addda25b363e6909fdbda09ab084026ba
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-05-28 17:53:40 +0530

    feat: Navigate to a new screen with Push and Pop

--- state_management_demo/lib/main.dart
+++ state_management_demo/lib/main.dart
@@ -86,6 +86,7 @@ class MyApp extends StatelessWidget {
             ParentWidget(),
             ParentWidgetC(),
             MainScreen(),
+            FirstRoute(),
           ],
         ),
       ),
@@ -373,3 +374,41 @@ class DetailScreen extends StatelessWidget {
   }
 }
 
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      child: Center(
+        child: RaisedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute())
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
