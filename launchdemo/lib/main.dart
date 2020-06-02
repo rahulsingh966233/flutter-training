f03968f7d40236cc91fda3ae70ec083cbdf64d31
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-06-02 12:42:38 +0530

    feat: Ad mapEventToState function

--- launchdemo/lib/main.dart
+++ launchdemo/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
