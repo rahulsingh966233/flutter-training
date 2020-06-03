405d4ca0127b48539f84f0b0be10ddd82d1a32b5
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-06-03 13:14:44 +0530

    feat: Implement pause and start methods

--- flutter_timer/lib/bloc/timer_bloc.dart
+++ flutter_timer/lib/bloc/timer_bloc.dart
@@ -1,15 +1,56 @@
-import 'package:bloc/bloc.dart';
-//import 'package:flutter_timer/bloc/bloc.dart';
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:fluttertimer/bloc/timer_event.dart';
 import 'package:fluttertimer/bloc/timer_state.dart';
+import 'package:fluttertimer/ticker.dart';
+import 'package:bloc/bloc.dart';
 
 // TimerEvent is declaration of 4 methods for pre
 class TimerBloc extends Bloc<TimerEvent, TimerState>{
+  final Ticker _ticker;
+  final int _duration = 60;
+
+  StreamSubscription<int> = _tickerSubscription;
+
+  TimerBloc({@required Ticker ticker}):
+      assert(ticker != null),
+  _ticker = ticker;
+
   @override
-  TimerState get initialState => throw UnimplementedError();
+  TimerState get initialState => Ready(_duration);
 
   @override
   Stream<TimerState> mapEventToState(TimerEvent event) async*{
-    throw UnimplementedError();
+        if(event is Start){
+          yield* _mapStartToState(event);
+        }else if(event is Pause){
+          yield* _mapPauseToState(event);
+        }
+        else if(event is Tick){
+          yield* _mapTickToState(event);
+        }
+  }
+
+  @override
+  Future<void> close(){
+    _tickerSubscription?.cancel();
+    return super.close();
+  }
+
+  Stream<TimerState> _mapStartToState(Start start) async*{
+    yield Running(start.duration);
+    _tickerSubscription?.cancel();
+    _tickerSubscription = _ticker.tick(ticks: start.duration).listen((duration) => add(Tick(duration: duration)));
+  }
+
+  Stream<TimerState> _mapPauseToState(Pause pause) async* {
+    if (state is Running) {
+      _tickerSubscription?.pause();
+      yield Paused(state.duration);
+    }
+  }
+
+  Stream<TimerState> _mapTickToState(Tick tick) async* {
+    yield tick.duration > 0 ? Running(tick.duration) : Finished();
   }
 }
