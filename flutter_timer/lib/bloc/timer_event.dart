bc2c57b0716c356449ca3af32ca71148f723515f
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-06-03 11:56:43 +0530

    feat: Create abstract class

---
+++ flutter_timer/lib/bloc/timer_event.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+abstract class TimerEvent extends Equatable {
+  const TimerEvent();
+
+  @override
+  List<Object> get props => []
+}
+
